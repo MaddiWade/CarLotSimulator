905db13cfcbceeca2adb975512b2e8bfe9a7c008
Maddi Wade <Madelinenwade@gmail.com>
2022-02-21 12:27:04 -0500

    finished!!!!

---
+++ CarLotSimulatorApp/CarLotSimulator/Car.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarLotSimulator
+{
+     public class Car
+    {
+        public int Year { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string EngineNoise { get; set; }
+        public string HonkNoise { get; set; }
+        public bool IsDriveable { get; set; } = true;
+
+        public Car()
+        {
+
+        }
+
+        public Car(int year, string make, string model, string engineNoise, string honknoise, bool isDriveable = true)
+        {
+            Year = year;
+            Make = make;
+            Model = model;
+            EngineNoise = engineNoise;
+            HonkNoise = honknoise;
+            IsDriveable = isDriveable;
+        }
+
+        public void MakeEngineNoise()
+        {
+            Console.WriteLine(EngineNoise);
+        }
+        
+        public void MakeHornNoise()
+        {
+            Console.WriteLine(HonkNoise);
+        }
+        
+    }
+    
+
+}
--- CarLotSimulatorApp/CarLotSimulator/Program.cs
+++ CarLotSimulatorApp/CarLotSimulator/Program.cs
@@ -1,11 +1,43 @@
 ﻿using System;
-
+using System.Collections.Generic;
 namespace CarLotSimulator
 {
     class Program
     {
         static void Main(string[] args)
         {
+            CarLot lot = new CarLot();
+            
+
+            Car car1 = new Car();
+            car1.Year = 2017;
+            car1.Make = "Honda";
+            car1.Model = "Accord";
+            car1.EngineNoise = "Vroom Vroom";
+            car1.HonkNoise = "beep beep";
+            car1.IsDriveable = true;
+           
+            car1.MakeEngineNoise();
+            car1.MakeHornNoise();
+            lot.CarList.Add(car1);
+
+            Car Becky = new Car() { Year = 1990, Make = "Ford", Model = "Explorer", EngineNoise = "Rattel", HonkNoise = "HONK", IsDriveable = false };
+
+            Becky.MakeEngineNoise();
+            Becky.MakeHornNoise();
+            lot.CarList.Add(Becky);
+
+            Car HotRod = new Car(1908, "Ford", "T", "Loud af", "Old fashion honk", false);
+
+            HotRod.MakeEngineNoise();
+            HotRod.MakeHornNoise();
+            lot.CarList.Add(HotRod);
+
+            foreach(var car in lot.CarList)
+            {
+                Console.WriteLine($"Year: {car.Year} Make: {car.Make} Model: {car.Model} Engine Noise: {car.EngineNoise} Honk Noise: {car.HonkNoise}, does is drive? {car.IsDriveable}");
+            }
+
             //TODO
 
             //Create a seperate class file called Car
